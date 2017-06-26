e8bb56c7f2cc704694748d879b9c7396fd81209d
Benjamin ONeill <boneill1292@gmail.com>
2017-06-26 13:09:19 -0500

    adding oauth, but cant get the injection to work

---
+++ SecretSantaApp/Auth0Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaApp
+{
+  public class Auth0Settings
+  {
+    public string Domain { get; set; }
+
+    public string CallbackUrl { get; set; }
+
+    public string ClientId { get; set; }
+
+    public string ClientSecret { get; set; }
+  }
+}
---
+++ SecretSantaApp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSantaApp.Controllers
+{
+  /// <summary>
+  /// For the Login, you will need to return a ChallengeResult and specify "Auth0" as the authentication scheme which will be challenged. This will invoke the OIDC middleware you registered in the Configure method.
+  ///After the OIDC middleware has signed the user in, the user will automatically be signed into the cookie middleware as well to authenticate them on subsequent requests.So, for the Logout action you will need to sign the user out of both the OIDC and the cookie middleware:
+  /// </summary>
+  public class AccountController : Controller
+  {
+    public IActionResult Login()
+    {
+      return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = "/" });
+    }
+
+    public async Task Logout()
+    {
+      await HttpContext.Authentication.SignOutAsync("Auth0", new AuthenticationProperties
+      {
+        RedirectUri = Url.Action("Index", "Home")
+      });
+      await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+  }
+}
