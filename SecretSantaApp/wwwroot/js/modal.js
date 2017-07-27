1f6c35648ced46ba550c8d8122357a0048707533
Benjamin ONeill <boneill1292@gmail.com>
2017-07-27 09:22:01 -0500

    modal fixes?

--- SecretSantaApp/wwwroot/js/modal.js
+++ SecretSantaApp/wwwroot/js/modal.js
@@ -1,23 +1,48 @@
 ﻿// modal.js
 
 $(function () {
-  //init modal dialog
-  // attach modal-container bootstrap attributes to links with .modal-link class.
-  // when a link is clicked with these attributes, bootstrap will display the href content ina  modal dialog.
-  $('body').on('click', '.modal-link', function (e) {
-    e.preventDefault();
-    $(this).attr('data-target', '#modal-container');
-    $(this).attr('data-toggle', 'modal');
-  });
+    //init modal dialog
+    // attach modal-container bootstrap attributes to links with .modal-link class.
+    // when a link is clicked with these attributes, bootstrap will display the href content ina  modal dialog.
+    $('body').on('click', '.modal-link', function (e) {
+        e.preventDefault();
+        $(this).attr('data-target', '#modal-container');
+        $(this).attr('data-toggle', 'modal');
+    });
 
-  //attach a listener to .modal-close-btn so when button is pressed the modal disappears
-  $('body').on('click', '.modal-close-btn', function () {
-    $('#modal-container').modal('hide');
-  });
+    //attach a listener to .modal-close-btn so when button is pressed the modal disappears
+    $('body').on('click', '.modal-close-btn', function () {
+        $('#modal-container').modal('hide');
+    });
 
-  //clear modal cache, so that new content can be loaded
-  $('#modal-container').on('hidden.bs.modal', function () {
-    $(this).removeData('bs.modal');
-  });
+    //clear modal cache, so that new content can be loaded
+    $('#modal-container').on('hidden.bs.modal', function () {
+        $(this).removeData('bs.modal');
+    });
+
+
+
+
+    //Clear the other modal?
+    $('body').on('click', '.modal-link', function (e) {
+        e.preventDefault();
+        console.log("new modal?");
+        $(this).attr('data-target', '#myModal');
+        $(this).attr('data-toggle', 'modal');
+    });
+
+    //attach a listener to .modal-close-btn so when button is pressed the modal disappears
+    $('body').on('click', '.modal-close-btn', function () {
+        $('#myModal').modal('hide');
+    });
+
+    //clear modal cache, so that new content can be loaded
+    $('#myModal').on('hidden.bs.modal', function () {
+        $(this).removeData('bs.modal');
+    });
+
+    $('#myModal').on('hidden.bs.modal', function () {
+        $(this).removeData('bs.modal');
+    });
 
 })
