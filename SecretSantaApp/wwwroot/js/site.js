ba64cf02261fae84f695e936d91e22c0fed252e2
Benjamin ONeill <boneill1292@gmail.com>
2017-07-27 08:20:53 -0500

    is it working? pushed code to javascript might have helped

--- SecretSantaApp/wwwroot/js/site.js
+++ SecretSantaApp/wwwroot/js/site.js
@@ -1,1 +1,27 @@
 ﻿// Write your Javascript code.
+
+
+//GroupHome View
+//load the rules partial on the right
+//function LoadRules() {
+//   // ClosePopup();
+//    window.LayoutLoadPartial('rulesdiv', '@grouprulesurl', { 'label': 'Loading Rules...' });
+//}
+
+////Load the chat partial view
+//function LoadChat() {
+//    //ClosePopup();
+//    window.LayoutLoadPartial('chatdiv', '@chaturl', { 'label': 'Loading Chat...' });
+//}
+
+function ClosePopup() {
+    $('#modal-container').modal('hide');
+}
+
+function ToggleChat() {
+    $('#chatdiv').toggle();
+}
+
+function LoadDetails() {
+    console.log("clicked details");
+}
